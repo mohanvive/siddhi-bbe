3ed1ea1580fa365e4cf7d854772b3158fcf95a74
Mohan <mohan.vive@gmail.com>
2019-07-15 19:00:21 +0530

    Disable commands temporary manner

--- tools/build-bbe.sh
+++ tools/build-bbe.sh
@@ -3,15 +3,16 @@ echo ".....Building BBE Site....."
 mkdir -p $2
 export GOPATH=$3
 go get github.com/russross/blackfriday
-rm target/dependencies/siddhi-examples -rf
 
 #get BBE from the language master branch
-rm ballerina-lang -rf
-git clone https://github.com/ballerina-platform/ballerina-lang
-git --git-dir=ballerina-lang/.git --work-tree=ballerina-lang/ checkout v0.991.0
-mkdir -p target/dependencies/ballerina-examples/
-mv ballerina-lang/examples target/dependencies/ballerina-examples/examples/
-rm ballerina-lang -r
+#---- Disable temporary ---
+#rm target/dependencies/siddhi-examples -rf
+#rm ballerina-lang -rf
+#git clone https://github.com/ballerina-platform/ballerina-lang
+#git --git-dir=ballerina-lang/.git --work-tree=ballerina-lang/ checkout v0.991.0
+#mkdir -p target/dependencies/ballerina-examples/
+#mv ballerina-lang/examples target/dependencies/ballerina-examples/examples/
+#rm ballerina-lang -r
 
 go run tools/siddhiByExample/tools/generate.go "target/dependencies/siddhi-examples" $2
 echo "....Completed building BBE Site...."
